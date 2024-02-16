b84229bbfafd0f888d11c26cdaa6f1e83a8f9886
Alex <adrysdale@protonmail.com>
2024-02-16 10:00:47 +0000

    Updated to Python 3.12

--- shell.nix
+++ shell.nix
@@ -2,14 +2,16 @@
 pkgs.mkShell {
   NIX_SHELL_PRESERVE_PROMPT=1;
   buildInputs = [
-    pkgs.python311Packages.numpy
+    pkgs.python312Packages.numpy
+    pkgs.python312Packages.matplotlib
+    pkgs.python312Packages.h5py
     pkgs.stdenv.cc.cc.lib
-    pkgs.libz
+    pkgs.zlib
   ];
   shellHook = ''
     export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
       pkgs.stdenv.cc.cc
-      pkgs.libz
+      pkgs.zlib
     ]}
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/gcc/x86_64-pc-linux-gnu/13/"
   '';
