2bd6bd86aeae2b589e9b4c3395e50de4b9353c58
Alex <adrysdale@protonmail.com>
2024-04-30 09:54:33 +0100

    Added batch script for pareto front

---
+++ scripts/single_entry_from_pareto_front_sbatch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash --login
+
+# Job name
+#SBATCH --job-name=SINGLE_FROM_PARETO.%J
+# Job stdout file
+#SBATCH --output=phys_0d_opt.out.%J
+# Job stderr file
+#SBATCH --error=phys_0d_opt.err.%J
+# Number of nodes
+#SBATCH --nodes=1
+# Number of tasks
+#SBATCH --ntasks=1
+# Number of CPUs per task
+#SBATCH --cpus-per-task=25
+# Parition
+#SBATCH --partition=compute
+# Time Limit (1440 = 24hrs)
+#SBATCH --time=2880
+# Account name
+#SBATCH --account=scw1706
+# Email alerts
+#SBATCH --mail-user=2026353@swansea.ac.uk
+#SBATCH --mail-type=ALL
+
+module load singularity/3.8.5
+
+# START, NUM and TOTAL are environment variables
+# as sbatch scripts don't allow for command line arguments.
+
+srun singularity exec --bind "$(pwd)":/app cl0.sif \
+python3 scripts/single_entry_from_pareto_front.py \
+--start ${START} --total ${TOTAL} --num_workers ${NUM} --log WARNING
